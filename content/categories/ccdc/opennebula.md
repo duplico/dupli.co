0c815b23caae34d6f3ba9e06d2a29d9cd2b862c9
George Louthan <duplico@dupli.co>
2025-03-28 21:14:10 -0500

    WIP commit

---
+++ content/categories/ccdc/opennebula.md
@@ -0,0 +1,6 @@
+Title: How we use OpenNebula for Southwest CCDC
+Category: CCDC
+Date: 2024-03-27
+Status: draft
+
+one-foo
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,21 +1,33 @@
 AUTHOR = 'george | duplico'
 SITENAME = 'George Louthan | duplico'
 SITEURL = ""
+TIMEZONE = 'America/Chicago'
+DEFAULT_LANG = 'en'
+THEME = "notmyidea"
 
 PATH = "content"
-
-TIMEZONE = 'America/Chicago'
-
-DEFAULT_LANG = 'en'
-
 STATIC_PATHS = [
     "images",
     "_meta",
 ]
-
 EXTRA_PATH_METADATA = {
     "_meta/favicon.ico": {"path": "favicon.ico"},
 }
+
+PLUGINS = [
+    "photos",
+]
+
+PHOTO_LIBRARY = "/home/george/project/dupli.co/photos"
+PHOTO_GALLERY = (1024, 768, 80)
+PHOTO_ARTICLE = (760, 506, 80)
+PHOTO_THUMB = (192, 144, 60)
+PHOTO_SQUARE_THUMB = True # Crop to square
+PHOTO_RESIZE_JOBS = 0 # Try to detect CPU count and start CPU_COUNT+1 jobs
+PHOTO_WATERMARK = True
+PHOTO_WATERMARK_TEXT = "@dupli.co"
+PHOTO_INLINE_GALLERY_ENABLED = True
+PHOTO_INLINE_GALLERY_TEMPLATE = "inline_gallery"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -29,10 +41,7 @@ LINKS = (
     ("Starlight Concerts", "https://www.starlightconcerts.org/"),
     ("Southwest CCDC", "https://www.southwestccdc.com/"),
     ("Queercon", "https://queercon.org/"),
-#     ("Pelican", "https://getpelican.com/"),
-#     ("Python.org", "https://www.python.org/"),
-#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-#     ("You can modify those links in your config file", "#"),
+    ("badge.lgbt", "https://badge.lgbt/"),
 )
 
 # Social widget
